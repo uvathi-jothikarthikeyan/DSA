b83a7305eff4ff8576a598484e318551ccd93f06
uvathi-jothikarthikeyan <uvathiuvathi07@gmail.com>
2025-12-16 21:55:53 +0530

    Arrays

---
+++ src/Arrays/BestTimeToSell.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class BestTimeToSell {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
